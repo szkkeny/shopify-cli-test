4f3c9f2a1b77eb3d6c10a6d8917126bebc48c2d9
szkkeny <kenya15fg@gmail.com>
2020-12-15 07:43:05 +0900

    add components

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,40 @@
-import fetch from "node-fetch";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import App, { Container } from "next/app";
-import { AppProvider } from "@shopify/polaris";
-import { Provider } from "@shopify/app-bridge-react";
-import Cookies from "js-cookie";
-import "@shopify/polaris/dist/styles.css";
-import translations from "@shopify/polaris/locales/en.json";
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/dist/styles.css';
+import translations from '@shopify/polaris/locales/en.json'
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from 'js-cookie';
+import { CustomClientRouter } from '../components/ClientRouter';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  fetch: fetch,
   fetchOptions: {
-    credentials: "include",
+    credentials: 'include'
   },
 });
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    const shopOrigin = Cookies.get("shopOrigin");
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
     return (
-      <AppProvider i18n={translations}>
-        <Provider
-          config={{
-            apiKey: API_KEY,
-            shopOrigin: shopOrigin,
-            forceRedirect: true,
-          }}
-        >
-          <ApolloProvider client={client}>
-            <Component {...pageProps} />
-          </ApolloProvider>
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <CustomClientRouter />
+          <AppProvider i18n={translations}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
         </Provider>
-      </AppProvider>
+      </React.Fragment>
     );
   }
 }
